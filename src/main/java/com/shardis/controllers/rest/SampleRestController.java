63d3fe784b413fda4076b20115753e2b4083a7af
rurik <urrast@yandex.ru>
2017-07-08 23:51:05 +0300

    build works fine

--- src/main/java/com/shardis/controllers/rest/SampleRestController.java
+++ src/main/java/com/shardis/controllers/rest/SampleRestController.java
@@ -1,14 +1,10 @@
 package com.shardis.controllers.rest;
 
 
-import com.google.common.collect.Lists;
-import com.shardis.dto.user.UserPost;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Date;
 
 /**
  * Created by Tomasz Kucharzyk
@@ -19,33 +15,9 @@ import java.util.List;
 public class SampleRestController {
 
     @RequestMapping("/test")
-    public List<String> test() {
-        return Lists.newArrayList("Vue.js", "is", "great");
+    public String test() {
+        return "test " + new Date().toString();
     }
 
-    @RequestMapping("/fail")
-    public List<String> fail() {
-        throw new RuntimeException("method failed");
-    }
 
-    @RequestMapping("/user/{userId}/posts")
-    public List<UserPost> userPosts(@PathVariable("userId") Long userId) {
-        List<UserPost> posts = Lists.newArrayList();
-        for (long i = 1; i <= 8; i++) {
-            posts.add(new UserPost(i, "Post #" + i + " of user " + userId, "sample content #" + i, LocalDateTime.now()));
-        }
-        return posts;
-    }
-
-    @RequestMapping("/user/{userId}/settings")
-    public List<String> userSettings(@PathVariable("userId") Long userId) throws InterruptedException {
-        //Don't do that at home
-        Thread.currentThread().sleep(1000);
-
-        List<String> settings = Lists.newArrayList();
-        for (int i = 0; i < 10; i++) {
-            settings.add("Setting #" + i+" of user "+userId);
-        }
-        return settings;
-    }
 }
--- src/main/vuejs/app.vue
+++ src/main/vuejs/app.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <h1>{{msg}}</h1>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
   name: 'app',
-  components: {
-    Hello
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
   }
 }
 </script>
@@ -24,5 +24,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
 }
 </style>
